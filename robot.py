c5edc06085459e08afcef438d2596f8c81905a84
minchingtonak <alec.minch@gmail.com>
2018-07-17 04:18:41 +0000

    Add correct PWM output function, safety mode, input squaring

--- robot.py
+++ robot.py
@@ -1,12 +1,21 @@
 from controller import Controller
+from pwmhat import PWMHat
 import time
 
 cont = Controller(0)
+p = PWMHat(50)
+val = 0
 
+#534 max forward
+#320.5 mid neutral
+#107 max backward
 while 1:
 	if cont.get_B():
 		exit(0)
-
-	print(cont.get_RS_Y())
-	time.sleep(0.1)
+	if cont.get_LB() and cont.get_RB():
+		val = int(320.5 + (cont.get_LS_Y()**2 * 213.5))
+	else:
+		val = 50
+	p.drive(0, val)
+	print(val)
 
